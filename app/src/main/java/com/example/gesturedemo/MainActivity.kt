a44ef5f248f0ddb7aa892392b54d2a9f3ddf2242
Егор <15egor02@gmail.com>
2025-12-25 14:37:17 +0700

    Распознавание жестов перетаскивания с помощью PointerInputScope

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -3,13 +3,12 @@ package com.example.gesturedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,7 +18,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,69 +39,42 @@ import kotlin.math.roundToInt
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-
+            GestureDemoTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MainScreen()
+                }
+            }
         }
     }
 }
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    TapPressDemo(modifier)
-    DragDemo(modifier)
-    ClickDemo(modifier)
     PointerInputDrag(modifier)
-
 }
-
 
 @Composable
-fun PointerInputDrag(modifier: Modifier = Modifier) {
-    Box(modifier = modifier.fillMaxSize()) {
-        var xOffset by remember { mutableStateOf(0f) }
-        var yOffset by remember { mutableStateOf(0f) }
-        Box(
-            Modifier
-                .offset { IntOffset(xOffset.roundToInt(), yOffset.roundToInt()) }
-                .background(Color.Blue)
-                .size(100.dp)
-                .pointerInput(Unit) {
-                    detectDragGestures { _, distance ->
-                        xOffset += distance.x
-                        yOffset += distance.y
-                    }
-                }
-        )
-    }
+fun ClickDemo(modifier: Modifier = Modifier) {
+    var isBlue by remember { mutableStateOf(true) }
+
+    val bgColor = if (isBlue) Color.Blue else Color.DarkGray
+
+    Box(
+        modifier
+            .size(100.dp)
+            .background(bgColor)
+            .clickable {
+                isBlue = !isBlue
+            }
+    )
 }
 
-
-
-@Composable
-fun DragDemo(modifier: Modifier = Modifier) {
-    Box(modifier = modifier.fillMaxSize()) {
-
-        var xOffset by remember { mutableStateOf(0f) }
-
-        Box(
-            modifier = Modifier
-                .offset { IntOffset(xOffset.roundToInt(), 0) }
-                .size(100.dp)
-                .background(Color.Blue)
-                .draggable(
-                    orientation = Orientation.Horizontal,
-                    state = rememberDraggableState { distance ->
-                        xOffset += distance
-                    }
-                )
-        )
-    }
-}
 @Composable
 fun TapPressDemo(modifier: Modifier = Modifier) {
-    var textState by remember {mutableStateOf("Waiting ....")}
-    val tapHandler = { status : String ->
+    var textState by remember { mutableStateOf("Waiting ....") }
+
+    val tapHandler = { status: String ->
         textState = status
     }
     Column(
@@ -126,30 +99,50 @@ fun TapPressDemo(modifier: Modifier = Modifier) {
         Text(textState)
     }
 }
+
 @Composable
-fun ClickDemo(modifier: Modifier = Modifier) {
-    var colorState by remember { mutableStateOf(true)}
-    var bgColor by remember { mutableStateOf(Color.Blue) }
-    val clickHandler = {
-        colorState = !colorState
-        bgColor = if (colorState) {
-            Color.Blue
-        } else {
-            Color.DarkGray
-        }
+fun DragDemo(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        var xOffset by remember { mutableStateOf(0f) }
+        Box(
+            modifier = Modifier
+                .offset { IntOffset(xOffset.roundToInt(), 0) }
+                .size(100.dp)
+                .background(Color.Blue)
+                .draggable(
+                    orientation = Orientation.Horizontal,
+                    state = rememberDraggableState { distance ->
+                        xOffset += distance
+                    }
+                )
+        )
     }
-    Box(
-        modifier
-            .clickable { clickHandler() }
-            .background(bgColor)
-            .size(100.dp)
-    )
 }
 
+@Composable
+fun PointerInputDrag(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        var xOffset by remember { mutableStateOf(0f) }
+        var yOffset by remember { mutableStateOf(0f) }
+        Box(
+            Modifier
+                .offset { IntOffset(xOffset.roundToInt(), yOffset.roundToInt()) }
+                .background(Color.Blue)
+                .size(100.dp)
+                .pointerInput(Unit) {
+                    detectDragGestures { _, distance ->
+                        xOffset += distance.x
+                        yOffset += distance.y
+                    }
+                }
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GestureDemoTheme {
+        MainScreen()
     }
 }
